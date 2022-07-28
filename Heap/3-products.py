0d8cde9c5878b28f4fde35bd1614a079f34fbda9
Sundar Singh Pharswan <sundar.singh.pharswan@outlook.com>
2022-07-28 07:03:36 +0530

    product of 3 largest element in an array

--- Heap/3-products.py
+++ Heap/3-products.py
@@ -1,3 +1,4 @@
+
 class Solution:
     # @param A : list of integers
     # @return a list of integers
@@ -11,33 +12,46 @@ class Solution:
         if n == 2:
             return [-1, -1]
 
-        indx = n // 2
+        h, k = [], 3
+        for i in range(k):
+            h.append(A[i])
 
-        h = []
-        for i in range(n):
+        self.heapify(h, k, 0)
+
+        for i in range(k, n):
+            p = h[0] * h[1] * h[2]
+            res.append(p)
             self.insert(h, A[i])
-            if i > 1:
-                l = len(h)
-                m3 = h[2]
-                
-                m3 = max(h[2], h[3], h[4])
-                p = h[0] * h[1] * h[2]
-                res.append(p)
+
+        p = h[0] * h[1] * h[2]
+        res.append(p)
 
         return res
 
+    # to find top k largest element we can create min heap and each time for new element
+    # we can compare if that can be be largest, as smallest will be at root
     def insert(self, A, num):
-        indx = len(A)
-        A.append(num)
+        if num > A[0]:
+            A[0] = num
+            self.heapify(A, len(A), 0)
 
-        while indx > 0:
+    def heapify(self, A, n, i):
+        while i < n:
+            smallest = i
+            left = i * 2 + 1
+            right = i * 2 + 2
 
-            pi = (indx - 1) // 2
-            if A[indx] > A[pi]:
-                A[indx], A[pi] = A[pi], A[indx]
-                indx = pi
-            else:
+            if left < n and A[left] < A[smallest]:
+                smallest = left
+
+            if right < n and A[right] < A[smallest]:
+                smallest = right
+
+            if smallest == i:
                 break
 
+            A[smallest], A[i] = A[i], A[smallest]
+            smallest = i
 
-print(Solution().solve([1, 2, 3, 4, 5]))
+
+print(Solution().solve([10, 2, 13, 4]))
